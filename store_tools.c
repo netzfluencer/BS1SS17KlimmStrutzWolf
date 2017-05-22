c502b4b8b1626e4425de191372ad87976431f96f
astrutz <agsjustforfun@gmx.de>
2017-05-22 21:34:27 +0200

    Delete implementiert, Testaufrufe in der main

--- store_tools.c
+++ store_tools.c
@@ -5,23 +5,25 @@
 #include <stdio.h>
 #include "store_tools.h"
 
-int put(int key, char* value, struct key_value kv[]) {
-    for (int i = 0; i < sizeof(kv); i++) {
-        if (kv[i].key == key) {
-            if (kv[i].key == NULL) {
-                kv[i].key = key;
-                kv[i].value = value;
-            }
-            else {
-                int tmp = kv[i].key;
-                kv[i].key = key;
-                kv[i].value = value;
-                return tmp;
+
+    int put(int key, char* value, struct key_value kv[]){
+     for(int i = 0; i < sizeof(kv); i++){
+        if(kv[i].key == key){
+            int tmp = kv[i].key;
+            kv[i].key = key;
+            kv[i].value = value;
+            return tmp;
+        }
+     }
+        for(int j = 0; j < sizeof(kv); j++){
+            if(kv[j].key == NULL){
+                kv[j].key = key;
+                kv[j].value = value;
+                return 0;
             }
         }
+        return -1;
     }
-    return -1;
-}
 
 char* get(int key, struct key_value kv[]) {
     for (int i = 0; i < sizeof(kv); i++) {
