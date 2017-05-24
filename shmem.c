a5c6fb69e87f26d086a3a0206c7bcf9245ab33b4
astrutz <agsjustforfun@gmx.de>
2017-05-24 14:12:24 +0200

    shmem ist baaack

--- shmem.c
+++ shmem.c
@@ -8,14 +8,14 @@ int shmem() {
     struct key_value *myPtr;
 
 
-    int shID = shmget(sm_key, kvlength * sizeof(struct key_value), IPC_CREAT | 0777);
+    int shID = shmget(sm_key, STORELENGTH * sizeof(struct key_value), IPC_CREAT | 0777);
 
     if(shID >= 0) {
         myPtr = (struct key_value *) shmat(shID, 0, 0);
             for(int i= 0; i < STORELENGTH; i++) {
                 myPtr[i] = kv[i];
             }
-        
+
     }
     else {
         perror("shmget");
