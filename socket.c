32003920dfd12a7687aabb111f838e607fc99dc0
astrutz <agsjustforfun@gmx.de>
2017-05-28 17:04:24 +0200

    Kommentare und Code formartiert

--- socket.c
+++ socket.c
@@ -1,14 +1,14 @@
 #include "socket.h"
 
 int strtoken(char *str, char *separator, char **token, int size) {
-    int i=0;
+    int i = 0;
     token[0] = strtok(str, separator);
-    while(token[i++] && i < size)
+    while (token[i++] && i < size)
         token[i] = strtok(NULL, separator);
     return (i);
 }
 
-int start(){
+int start() {
     /* Variablen für den Server */
     int sock;
     struct sockaddr_in server;
@@ -28,8 +28,7 @@ int start(){
     if (sock < 0) {
         perror("creating stream socket");
         exit(1);
-    }
-    else {
+    } else {
         printf("Socket created.\n");
     }
 
@@ -42,10 +41,9 @@ int start(){
     server.sin_addr.s_addr = htonl(INADDR_ANY);
     server.sin_port = htons(1996);
 
-    if(bind( sock, (struct sockaddr *) &server, sizeof(server))<0){
+    if (bind(sock, (struct sockaddr *) &server, sizeof(server)) < 0) {
         perror("Error on binding");
-    }
-    else {
+    } else {
         printf("Socket binded.\n");
     }
 
@@ -55,10 +53,9 @@ int start(){
 
 
     /* Auf Verbindung hören */
-    if(listen(sock, 5)<0){
+    if (listen(sock, 5) < 0) {
         perror("Error on listening");
-    }
-    else {
+    } else {
         printf("Listening...\n");
     }
 
@@ -69,19 +66,19 @@ int start(){
     socklen_t client_len;
     client_len = sizeof(client);
 
-    while(1) {
+    while (1) {
         fileDescriptor = accept(sock, (struct sockaddr *) &client, &client_len);
         printf("Connection!\n");
-        while(read(fileDescriptor, in, 2000) > 0) {
+        while (read(fileDescriptor, in, 2000) > 0) {
             // Daten vom Socket ==> in
 
             // Loeschen des "ENTER"s & des "Carriage Return" (letzte zwei Zeichen)
-            in[strlen(in)-1] = 0;
-            in[strlen(in)-1] = 0;
+            in[strlen(in) - 1] = 0;
+            in[strlen(in) - 1] = 0;
 
 
             // Check: Keine leere Eingabe
-            if(strlen(in)>0) {
+            if (strlen(in) > 0) {
 
                 // Splitting von Cmd, Key, Value
                 strtoken(in, " ", in_splitted, 3);
@@ -90,69 +87,51 @@ int start(){
                     // Prüfen ob der key Parameter existiert
                     if (in_splitted[1] != NULL) {
 
-                        //HIER GET FUNKTION AUFRUFEN
                         get(in_splitted[1], "");
 
                         strcpy(out, "cKey-Action: get\n");
-                    }
-                    else {
+                    } else {
                         strcpy(out, "Err on get: No key submitted\n");
                     }
-                }
-
-
-                else if(strcmp(in_splitted[0], "put") == 0) {
+                } else if (strcmp(in_splitted[0], "put") == 0) {
                     if (in_splitted[1] != NULL) {
                         // Prüfen ob ein value übergeben wurde
-                        if(in_splitted[2] != NULL) {
+                        if (in_splitted[2] != NULL) {
 
-                            // HIER PUT FUNKTION AUFRUFEN
                             put(in_splitted[1], in_splitted[2], "");
 
                             strcpy(out, "cKey-Action: put\n");
-                        }
-                        else {
+                        } else {
                             strcpy(out, "Err on put: No Value submitted\n");
                         }
-                    }
-                    else {
+                    } else {
                         strcpy(out, "Err on put: No key submitted\n");
                     }
-                }
-
-
-                else if(strcmp(in_splitted[0], "delete") == 0) {
+                } else if (strcmp(in_splitted[0], "delete") == 0) {
                     if (in_splitted[1] != NULL) {
 
-                        // HIER DELETE FUNKTION AUFRUFEN
-                            delete(in_splitted[1], "");
+                        delete(in_splitted[1], "");
 
                         strcpy(out, "cKey-Action: delete\n");
-                    }
-                    else {
+                    } else {
                         strcpy(out, "Err on delete: No key submitted\n");
                     }
-                }
-
-
-                else {
+                } else {
                     strcpy(out, "Err: Unknown command\n");
                 }
 
                 // "in_splitted" chars wieder loeschen und frei machen
-                memset(in_splitted[0],0,strlen(in_splitted[0]));
-                if (in_splitted[1] != NULL) memset(in_splitted[1],0,strlen(in_splitted[1]));
-                if (in_splitted[2] != NULL) memset(in_splitted[2],0,strlen(in_splitted[2]));
+                memset(in_splitted[0], 0, strlen(in_splitted[0]));
+                if (in_splitted[1] != NULL) memset(in_splitted[1], 0, strlen(in_splitted[1]));
+                if (in_splitted[2] != NULL) memset(in_splitted[2], 0, strlen(in_splitted[2]));
             }
-
-
 
 
             write(fileDescriptor, out, strlen(out)); // Daten vom Array out ==> Socket
 
             // "in" und "out" chars wieder loeschen und frei machen
-            memset(in,0,strlen(in));
-            memset(out,0,strlen(out));
+            memset(in, 0, strlen(in));
+            memset(out, 0, strlen(out));
         }
         close(fileDescriptor);
     }
