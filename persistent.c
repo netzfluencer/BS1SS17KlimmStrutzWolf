db7627e537c98ff51f5363f6f8044948604bb73d
astrutz <alexander_gabriel.strutz@smail.th-koeln.de>
2017-06-28 14:43:58 +0200

    Zwischenstand

--- persistent.c
+++ persistent.c
@@ -5,17 +5,23 @@
 #include "persistent.h"
 #include "socket.h"
 
-FILE *file;
-file = fopen("data.txt", "w+");
+FILE *dataFile;
+
+
 
 
 void auslesen(char key[], char resp[]){
-    while(fscanf(file, "%s %s", *key, *resp)){
-        
+    //opens file in mode r for read
+    dataFile = fopen("data.txt", "r");
+    while(fscanf(dataFile, "%s %s", key, resp) > 0){
+        //output data in a useful way
+        strcat(resp, );
+        fclose(dataFile);
     }
 
 }
 
-void schreiben(){
-
+void schreiben(char key[], char resp[]){
+    dataFile = fopen("data.txt", "w");
+    fprintf(dataFile, "%s %s\n", key, resp);
 }
